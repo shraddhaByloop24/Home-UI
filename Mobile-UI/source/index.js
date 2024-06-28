a03133ae57d36a97fcd40fdf47f69bbc28cd1144
shraddhabyloop24 <shraddhapatel4829907@gmail.com>
2024-06-28 17:33:20 +0530

    update

---
+++ Mobile-UI/source/index.js
@@ -0,0 +1,166 @@
+const dict = {
+    "en": {
+        "title": "Discover New Friends, Practice Language",
+        "description": "On Hablo Talk, you can make new friends and practice languages with native speakers!"
+    },
+    "fr": {
+        "title": "Découvrez de nouveaux amis, pratiquez les langues",
+        "description": "Sur Hablo Talk, vous pouvez vous faire de nouveaux amis et pratiquer les langues avec des locuteurs natifs !"
+    },
+    "es": {
+        "title": "Descubre nuevos amigos, practica idiomas",
+        "description": "¡En Hablo Talk, puedes hacer nuevos amigos y practicar idiomas con hablantes nativos!"
+    },
+    "de": {
+        "title": "Entdecke neue Freunde, Sprachen üben",
+        "description": "Auf Hablo Talk kannst du neue Freunde finden und Sprachen mit Muttersprachlern üben!"
+    },
+    "it": {
+        "title": "Scopri nuovi amici, pratica le lingue",
+        "description": "Su Hablo Talk, puoi fare nuovi amici e praticare le lingue con madrelingua!"
+    },
+    "ru": {
+        "title": "Откройте для себя новых друзей, занимайтесь языками",
+        "description": "На Hablo Talk вы можете завести новых друзей и заниматься языками с носителями языка!"
+    },
+    "ja": {
+        "title": "新しい友達を見つけて、言語を練習しよう",
+        "description": "Hablo Talkでは、新しい友達を作ったり、ネイティブスピーカーと言語を練習したりできます！"
+    },
+    "zh-CN": {
+        "title": "结识新朋友，练习语言",
+        "description": "在 Hablo Talk 上，您可以结识新朋友并与母语者练习语言！"
+    },
+    "ko": {
+        "title": "새로운 친구 만나고, 언어 연습하기",
+        "description": "Hablo Talk에서는 새로운 친구를 만들고 원어민과 언어를 연습할 수 있습니다!"
+    },
+    "ar": {
+        "title": "اكتشف أصدقاء جدد ، واستمتع بتعلم اللغة",
+        "description": "في Hablo Talk ، يمكنك تكوين أصدقاء جدد وممارسة اللغات مع الناطقين الأصليين!"
+    },
+    "hi": {
+        "title": "नए दोस्त बनाएं, भाषा का अभ्यास करें",
+        "description": "Hablo Talk में, आप नए दोस्त बना सकते हैं और मूल भाषीयों के साथ भाषाओं का अभ्यास कर सकते हैं!"
+    },
+    "pt": {
+        "title": "Descubra Novos Amigos, Pratique Idiomas",
+        "description": "No Hablo Talk, você pode fazer novos amigos e praticar idiomas com falantes nativos!"
+    },
+    "nl": {
+        "title": "Ontdek Nieuwe Vrienden, Oefen Talen",
+        "description": "Op Hablo Talk kun je nieuwe vrienden maken en talen oefenen met moedertaalsprekers!"
+    },
+    "tr": {
+        "title": "Yeni Arkadaşlar Keşfet, Dil Egzersizi Yap",
+        "description": "Hablo Talk'ta yeni arkadaşlar edinebilir ve ana dili konuşanlarla dil egzersizi yapabilirsiniz!"
+    },
+    "pl": {
+        "title": "Odkrywaj nowych przyjaciół, Ćwicz język",
+        "description": "Na Hablo Talk możesz zawierać nowe znajomości i ćwiczyć języki z native speakerami!"
+    },
+    "vi": {
+        "title": "Khám phá bạn mới, Luyện tập ngôn ngữ",
+        "description": "Trên Hablo Talk, bạn có thể kết bạn mới và luyện tập ngôn ngữ với người bản xứ!"
+    },
+    "el": {
+        "title": "Ανακαλύψτε Νέους Φίλους, Εξασκήστε τη Γλώσσα",
+        "description": "Στο Hablo Talk, μπορείτε να κάνετε νέους φίλους και να εξασκήσετε γλώσσες με φυσικούς ομιλητές!"
+    },
+    "sv": {
+        "title": "Upptäck Nya Vänner, Öva Språk",
+        "description": "På Hablo Talk kan du träffa nya vänner och öva språk med infödda talare!"
+    },
+    "cs": {
+        "title": "Objevujte nové přátele, Procvičujte jazyk",
+        "description": "Na Hablo Talk můžete najít nové přátele a procvičovat jazyky s rodilými mluvčími!"
+    },
+    "fi": {
+        "title": "Löydä uusia ystäviä, Harjoittele kieliä",
+        "description": "Hablo Talkissa voit tutustua uusiin ystäviin ja harjoitella kieliä äidinkielisten kanssa!"
+    },
+    "no": {
+        "title": "Oppdag Nye Venner, Øv Språk",
+        "description": "På Hablo Talk kan du møte nye venner og øve språk med morsmålere!"
+    },
+    "da": {
+        "title": "Opdag Nye Venner, Øv Sprog",
+        "description": "På Hablo Talk kan du møde nye venner og øve sprog med modersmålere!"
+    },
+    "hu": {
+        "title": "Fedezz fel új barátokat, Gyakorolj nyelveket",
+        "description": "A Hablo Talkon új barátokat szerezhetsz és nyelveket gyakorolhatsz a helyi beszélőkkel!"
+    },
+    "ro": {
+        "title": "Descoperiți Noi Prieteni, Exersați Limbile",
+        "description": "Pe Hablo Talk, puteți face noi prieteni și puteți exersa limbile cu vorbitorii nativi!"
+    },
+    "uk": {
+        "title": "Відкрийте для себе нових друзів, займайтеся мовами",
+        "description": "На Hablo Talk ви можете завести нових друзів і займатися мовами з носіями мови!"
+    },
+    "hr": {
+        "title": "Otkrijte nove prijatelje, Vježbajte jezik",
+        "description": "Na Hablo Talku možete upoznati nove prijatelje i vježbati jezike s izvornim govornicima!"
+    },
+    "sr": {
+        "title": "Upoznajte nove prijatelje, Vježbajte jezik",
+        "description": "Na Hablo Talku možete upoznati nove prijatelje i vježbati jezike s izvornim govornicima!"
+    },
+    "bs": {
+        "title": "Otkrijte nove prijatelje, Vježbajte jezik",
+        "description": "Na Hablo Talku možete upoznati nove prijatelje i vježbati jezike s izvornim govornicima!"
+    },
+    "mk": {
+        "title": "Откријте нови пријатели, Вежбајте јазик",
+        "description": "На Хабло Талку можете запознати нови пријатели и да вежбате јазици со матерните говорници!"
+    },
+    "sl": {
+        "title": "Odkrijte nove prijatelje, Vadite jezik",
+        "description": "Na Hablo Talku lahko spoznate nove prijatelje in vadite jezike s starši!"
+    },
+    "et": {
+        "title": "Avasta uusi sõpru, Keele harjutamine",
+        "description": "Hablo Talkis saate tutvuda uute sõpradega ja harjutada keeli emakeelsete kõnelejatega!"
+    },
+    "lt": {
+        "title": "Atraskite naujus draugus, Praktikuokite kalbas",
+        "description": "Hablo Talk'e galite susipažinti su naujais draugais ir praktikuoti kalbas su gimtakalbiais!"
+    },
+    "lv": {
+        "title": "Atklājiet jaunus draugus, Praktizējiet valodas",
+        "description": "Hablo Talkā jūs varat iepazīties ar jauniem draugiem un praktizēt valodas ar dzimtajiem runātājiem!"
+    },
+    "sk": {
+        "title": "Objavte nových priateľov, Cvičte jazyky",
+        "description": "Na Hablo Talk môžete spoznávať nových priateľov a cvičiť jazyky s rodilými rečníkmi!"
+    },
+    "fi": {
+        "title": "Löydä uusia ystäviä, Harjoittele kieliä",
+        "description": "Hablo Talkissa voit tutustua uusiin ystäviin ja harjoitella kieliä äidinkielisten kanssa!"
+    },
+    "is": {
+        "title": "Upptök nýrra vina, Æfing tungumála",
+        "description": "Á Hablo Talk getur þú kynnst nýjum vinum og æft tungumál með móðurmálum!"
+    }
+}
+
+
+window.onload = function () {
+    let deviceLanguage = navigator.language || navigator.userLanguage;
+
+    // Extract the language code
+    let languageCode = deviceLanguage.split("-")[0];
+
+    let data = {}
+
+    if (languageCode in dict) {
+        data = dict[languageCode]
+    } else {
+        data = dict["en"]
+    }
+
+    document.getElementById("title").innerHTML = data["title"];
+    document.getElementById("description").innerHTML = data["description"];
+    document.getElementById("heading2").innerHTML = "Download the application"; // Update this line
+};
